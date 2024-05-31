d6284633104145356d70ed9f932e642ba65dd467
phanhoang36 <121334872+phanhoang36@users.noreply.github.com>
2024-05-31 16:55:08 +0700

    Time: 89 ms (43.65%), Space: 95.6 MB (93.70%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
         int sum = 0;
         int num = prices[0];
         for(int i = 1; i < prices.size(); i++){
-            if(num >= prices[i]){
+            if(num > prices[i]){
                 num = prices[i];
             }
             else if(sum < prices[i] - num){
