abf7ad773fa040c4795f5498a7cc602d916255da
phanhoang36 <121334872+phanhoang36@users.noreply.github.com>
2024-06-13 21:51:48 +0700

    Time: 0 ms (100.00%), Space: 9.6 MB (74.80%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
       vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+          // khai báo biến i để lưu trữ giá trị 
         int freq[1001] = {0}, m = 0, i = 0;
           //đếm phần tử trong mảng
         for (int num : arr1){
@@ -12,6 +13,7 @@ public:
             while (freq[num]--)
                 arr1[i++] = num;
         }
+          // hiên tại của arr1 arr1 = [2, 2, 2, 1, 4, 3, 3, 9, 6]
           //duyệt lại mảng nếu > 0 => đang còn phần tử 
         for (int j = 0; j <= m; j++){
             while (freq[j]-- > 0)
